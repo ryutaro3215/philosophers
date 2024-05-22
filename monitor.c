8eec718c5f34d9834e8a17e1f39a5897c92e0d66
ryutaro3215 <ryutaro140515@icloud.com>
2024-05-22 18:07:06 +0900

    modified monitoring and routine

--- monitor.c
+++ monitor.c
@@ -6,79 +6,16 @@
 /*   By: ryutaro320515 <ryutaro320515@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 11:12:57 by ryutaro3205       #+#    #+#             */
-/*   Updated: 2024/05/16 19:25:57 by ryutaro3205      ###   ########.fr       */
+/*   Updated: 2024/05/22 17:54:56 by ryutaro3205      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo.h"
 
-void	print_message(t_philo *philo, char *str)
-{
-	size_t	time;
-
-	pthread_mutex_lock(&philo->env->print_mutex);
-	time = get_current_time() - philo->start_time;
-	if (!check_dead_flag(philo->env))
-		printf("%zu %zu %s\n", time, philo->id, str);
-	pthread_mutex_unlock(&philo->env->print_mutex);
-}
-
-bool	check_philo_dead(t_philo *philo, t_info *info, t_env *env)
-{
-	size_t	i;
-	size_t	time;
-
-	i = 0;
-	time = get_current_time();
-	pthread_mutex_lock(&env->dead_mutex);
-	while (i < info->philo_num)
-	{
-		if (time - philo[i].last_eat >= info->tt_die)
-		{
-			printf("%zu %zu %s\n", time - philo[i].start_time,
-				philo[i].id, DEAD);
-			env->is_dead = true;
-			pthread_mutex_unlock(&env->dead_mutex);
-			return (true);
-		}
-		i++;
-	}
-	pthread_mutex_unlock(&env->dead_mutex);
-	return (false);
-}
-
-bool	check_all_eat(t_philo *philo, t_info *info, t_env *env)
-{
-	size_t	i;
-	size_t	finish_eat_philo;
-
-	i = 0;
-	finish_eat_philo = 0;
-	if (info->must_eat == false)
-		return (false);
-	while (i < philo->info->philo_num)
-	{
-		pthread_mutex_lock(&env->eat_mutex);
-		if (philo[i].eat_count >= info->must_eat_count)
-			finish_eat_philo++;
-		pthread_mutex_unlock(&env->eat_mutex);
-		i++;
-	}
-	if (finish_eat_philo == info->philo_num)
-	{
-		pthread_mutex_lock(&env->dead_mutex);
-		printf("All philosophers have eaten %zu times\n", info->must_eat_count);
-		env->is_dead = true;
-		pthread_mutex_unlock(&env->dead_mutex);
-		return (true);
-	}
-	return (false);
-}
-
-bool	check_dead_flag(t_env *env)
+bool	check_dead(t_env *env)
 {
 	pthread_mutex_lock(&env->dead_mutex);
-	if (env->is_dead)
+	if (env->is_dead == true)
 	{
 		pthread_mutex_unlock(&env->dead_mutex);
 		return (true);
@@ -90,17 +27,12 @@ bool	check_dead_flag(t_env *env)
 void	*monitoring(void *arg)
 {
 	t_philo	*philo;
-	t_info	*info;
-	t_env	*env;
 
 	philo = (t_philo *)arg;
-	info = philo->info;
-	env = philo->env;
-	while (!check_dead_flag(env))
+	while (true)
 	{
-		if (check_all_eat(philo, info, env)
-			|| check_philo_dead(philo, info, env))
-			break ;
+		if (check_dead(philo->env) == true)
+			return (NULL);
 	}
 	return (NULL);
 }
--- routine.c
+++ routine.c
@@ -6,76 +6,46 @@
 /*   By: ryutaro320515 <ryutaro320515@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 13:59:06 by ryutaro3205       #+#    #+#             */
-/*   Updated: 2024/05/16 18:21:51 by ryutaro3205      ###   ########.fr       */
+/*   Updated: 2024/05/22 18:06:12 by ryutaro3205      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo.h"
 
-void	take_fork(t_philo *philo)
+void	dead_flag(t_env *env)
 {
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(philo->l_fork);
-		print_message(philo, L_FORK);
-		pthread_mutex_lock(philo->r_fork);
-		print_message(philo, R_FORK);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->r_fork);
-		print_message(philo, R_FORK);
-		pthread_mutex_lock(philo->l_fork);
-		print_message(philo, L_FORK);
-	}
-	return ;
+	pthread_mutex_lock(&env->dead_mutex);
+	env->is_dead = true;
+	pthread_mutex_unlock(&env->dead_mutex);
 }
 
 void	philo_eat(t_philo *philo)
 {
-	if (philo->env->is_dead)
-		return ;
-	if (philo->info->philo_num == 1)
-	{
-		my_usleep(philo->info->tt_die);
-		return ;
-	}
-	take_fork(philo);
 	pthread_mutex_lock(&philo->env->eat_mutex);
-	philo->is_eating = true;
-	philo->last_eat = get_current_time();
-	print_message(philo, EATING);
-	my_usleep(philo->info->tt_eat);
-	philo->is_eating = false;
+	if (!check_dead(philo->env))
+		printf("philo[%zu] eat_count[%zu]\n", philo->id, philo->eat_count);
 	philo->eat_count++;
+	if (philo->eat_count == philo->info->must_eat_count ||
+		philo->info->must_eat == true)
+		dead_flag(philo->env);
 	pthread_mutex_unlock(&philo->env->eat_mutex);
-	pthread_mutex_unlock(philo->r_fork);
-	pthread_mutex_unlock(philo->l_fork);
 }
 
 void	philo_sleep(t_philo *philo)
 {
-	print_message(philo, SLEEPING);
+	printf("philo[%zu] %s\n", philo->id, SLEEPING);
 	my_usleep(philo->info->tt_sleep);
-}
-
-void	philo_think(t_philo *philo)
-{
-	print_message(philo, THINKING);
 }
 
 void	*routine(void *arg)
 {
 	t_philo	*philo;
-	t_env	*env;
 
 	philo = (t_philo *)arg;
-	env = philo->env;
-	while (!check_dead_flag(env))
+	while (!check_dead(philo->env))
 	{
 		philo_eat(philo);
 		philo_sleep(philo);
-		philo_think(philo);
 	}
 	return (NULL);
 }
